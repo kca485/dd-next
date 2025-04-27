54484c45103e1375d214b9cbae8579ab3bd1a38a
Karntino Areros <karntino.c.areros@gmail.com>
2025-04-28 01:18:10 +0700

    add a few log

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -3,8 +3,16 @@ import { redirect } from "next/navigation";
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const tokenHash = url.searchParams.get("token_hash");
-  await fetch(
+  const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/auth/confirm?token_hash=${tokenHash}`,
   );
+
+  console.log("res", res);
+  if (!res.ok) {
+    console.error("Auth response is not ok");
+    redirect("login");
+  }
+
+  console.log("auth", res.json());
   redirect("/");
 }
